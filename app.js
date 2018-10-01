4581ca08f100b1ebee475d15108a71c6a2f6c5c5
Teerayut36 <t.yis0204@gmail.com>
2018-10-01 18:06:50 +0700

    edit4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
+//var db =pgp(process.env.DATABASE_URL);
+var db = pgp('postgres://snzadxroisaukx:adf58423ee26b3ecd30a3e8f1076764acc2365d3cd4eb0899a2d6c4d4c18cd8d@ec2-54-243-147-162.compute-1.amazonaws.com:5432/dbvugdbvctd4pk?ssl=true');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
