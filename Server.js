0121d3aa8b2f43534297561c094a39d4b3c673f4
Teerayut36 <t.yis0204@gmail.com>
2018-10-01 16:10:07 +0700

    commid1

--- Server.js
+++ Server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var pgp = require('pg-promise')();
 var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
-
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 /////app.use(express.static('static'));
 ////app.use(express.static('static/about.html'));
 app.set('view engine', 'ejs');
@@ -35,6 +38,18 @@ app.get('/products', function (req, res) {
         })
 
 });
+app.get('/products/:pid', function (req, res) {
+   var pid =req.params.pid;
+   var sql = "select * from products where id=" + pid;
+   db.any(sql)
+        .then(function (data) {
+             res.render('pages/products_edit', { products: data[0] })
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+
+        }) 
+   });
 app.get('/users/:id', function (req, res) {
     var id = req.param ('id');
     var sql = 'select * from users';
@@ -56,5 +71,18 @@ app.get('/users/:id', function (req, res) {
 app.get('/index', function (req, res) {
     res.render('pages/index');
 });
+///update
+app.post('/products/update' ,function (req, res){
+  var id = req.body.id;
+  var title = req.body.title;
+  var price = req.body.price;
+  var sql = `update product set title = ${title},price = ${price} where id = ${id} ` ;
+  ////db.none
+  console.log('UPDATE:' + sql);
+  res.redirect('/products');
+
+
+});
+
 console.log('App is runing at http://localhost:3000/');
 app.listen(3000);
